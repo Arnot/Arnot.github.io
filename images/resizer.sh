45ed74a77e730e613fc511dfdd60a8ade4985fff
Arno Tiemersma <arno.tiemersma@gmail.com>
2014-07-21 14:42:10 +0200

    new post, Hong Kong, Yangshuo

--- images/resizer.sh
+++ images/resizer.sh
@@ -1,3 +1,3 @@
 mkdir thumbs
-mogrify -resize 1200x -gravity center -extent 1200x -format JPG *.JPG
-#mogrify -resize 1200x -gravity center -extent 1200x -format JPG -path thumbs *.JPG
+mogrify -verbose -resize 500x -gravity center -extent 500x -format JPG -path thumbs *.JPG
+mogrify -verbose -resize 1200x -gravity center -extent 1200x -format JPG *.JPG
